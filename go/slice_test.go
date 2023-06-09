bf499f6757ec8cae22fe38710138eb7b579fad40
soup <lqh348659137@outlook.com>
2023-06-09 17:32:21 +0800

    commit go

---
+++ go/slice_test.go
@@ -0,0 +1,50 @@
+package _go
+
+import (
+	"math/rand"
+	"runtime"
+	"testing"
+	"time"
+)
+
+func generateWithCap(n int) []int {
+	rand.Seed(time.Now().UnixNano())
+	nums := make([]int, 0, n)
+	for i := 0; i < n; i++ {
+		nums = append(nums, rand.Int())
+	}
+	return nums
+}
+
+func printMem(t *testing.T) {
+	t.Helper()
+	var rtm runtime.MemStats
+	runtime.ReadMemStats(&rtm)
+	t.Logf("%.2f MB", float64(rtm.Alloc)/1024./1024.)
+}
+
+func testLastChars(t *testing.T, f func([]int) []int) {
+	t.Helper()
+	ans := make([][]int, 0)
+	for k := 0; k < 100; k++ {
+		origin := generateWithCap(128 * 1024) // 1M
+		ans = append(ans, f(origin))
+		//显示地调用 runtime.GC()，效果会更加地明显
+		//runtime.GC()
+	}
+	printMem(t)
+	_ = ans
+}
+
+func lastNumsBySlice(origin []int) []int {
+	return origin[len(origin)-2:]
+}
+
+func lastNumsByCopy(origin []int) []int {
+	result := make([]int, 2)
+	copy(result, origin[len(origin)-2:])
+	return result
+}
+
+func TestLastCharsBySlice(t *testing.T) { testLastChars(t, lastNumsBySlice) }
+func TestLastCharsByCopy(t *testing.T)  { testLastChars(t, lastNumsByCopy) }
