8b0bc139ae904c5e412773750dbb34f273ec27d2
soup <lqh348659137@outlook.com>
2023-08-22 20:17:38 +0800

    53

--- go/array.go
+++ go/array.go
@@ -456,3 +456,21 @@ func rotate(matrix [][]int) {
 		}
 	}
 }
+
+// 53. 最大子数组和
+func maxSubArray(nums []int) int {
+	length := len(nums)
+	if length == 0 {
+		return 0
+	}
+	max := nums[0]
+	for i := 1; i < length; i++ {
+		if nums[i]+nums[i-1] > nums[i] {
+			nums[i] = nums[i] + nums[i-1]
+		}
+		if nums[i] > max {
+			max = nums[i]
+		}
+	}
+	return max
+}
