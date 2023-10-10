7fb093c31a48b982d78c5b341c3973cad1693e4f
soup <lqh348659137@outlook.com>
2023-10-10 11:17:19 +0800

    88

---
+++ go/top_150.go
@@ -0,0 +1,23 @@
+//https://leetcode.cn/studyplan/top-interview-150/
+
+package _go
+
+// 88. 合并两个有序数组
+func merge2(nums1 []int, m int, nums2 []int, n int) {
+	i, j, k := m-1, n-1, m+n-1
+	for i >= 0 && j >= 0 {
+		if nums1[i] >= nums2[j] {
+			nums1[k] = nums1[i]
+			i--
+		} else {
+			nums1[k] = nums2[j]
+			j--
+		}
+		k--
+	}
+	for j >= 0 {
+		nums1[k] = nums2[j]
+		j--
+		k--
+	}
+}
